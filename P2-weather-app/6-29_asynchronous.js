5e24fcbcd1c132daa3426041130427f9ba8b8a68
zhsam <samchen0727@gmail.com>
2019-03-25 00:28:04 +0800

    Update: 6-29 asynchronous example.

---
+++ P2-weather-app/6-29_asynchronous.js
@@ -0,0 +1,11 @@
+console.log('starting')
+
+setTimeout(()=>{
+    console.log('2 Second Timer')    
+}, 2000) // 2s
+
+setTimeout(()=>{
+    console.log('0 Second Timer')    
+}, 0) // 2s
+
+console.log('stopping')
