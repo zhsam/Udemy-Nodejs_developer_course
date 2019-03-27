ca88f10b231e0ee2cf44bdedd6fbc1397dd1746a
zhsam <samchen0727@gmail.com>
2019-03-28 01:32:19 +0800

    Update: 8-48 creating a search form.

--- P3-web-server/public/js/app.js
+++ P3-web-server/public/js/app.js
@@ -1,11 +1,10 @@
 console.log('Client side javascript file is loaded.')
 
-fetch('http://puzzle.mead.io/puzzle').then((response)=>{
-    response.json().then((data)=>{
-        console.log(data)
-    })
-})
-
+// fetch('http://puzzle.mead.io/puzzle').then((response)=>{
+//     response.json().then((data)=>{
+//         console.log(data)
+//     })
+// })
 
 //
 // Goal: Fetch weather!
@@ -16,13 +15,42 @@ fetch('http://puzzle.mead.io/puzzle').then((response)=>{
 //      - If no error property, print location and forecast
 // 3. Refresh the browser and test your work
 
-fetch('http://localhost:3000/weather?address=boston').then((response)=>{
-    response.json().then((data)=>{
-        if(data.error){
-            console.log(data.error)
-        } else{
-            console.log(data.location)
-            console.log(data.forecast)
-        }
+// fetch('http://localhost:3000/weather?address=boston').then((response)=>{
+//     response.json().then((data)=>{
+//         if(data.error){
+//             console.log(data.error)
+//         } else{
+//             console.log(data.location)
+//             console.log(data.forecast)
+//         }
+//     })
+// })
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+
+// e --> event
+weatherForm.addEventListener('submit', (e)=>{
+    e.preventDefault() // prevent refresh the browser
+    const location = search.value
+    
+    const url = 'http://localhost:3000/weather?address=' + location
+
+    fetch(url).then((response)=>{
+        response.json().then((data)=>{
+            if(data.error){
+                console.log(data.error)
+            } else{
+                console.log(data.location)
+                console.log(data.forecast)
+            }
+        })
     })
 })
+
+//
+// Goal: Use input value to get weather
+//
+// 1. Migrate fetch call into the submit call back
+// 2. Use the search text as the address query string
+// 3. Submit the form with no value inside invalid/valid
