d46549fc8df0ceddb9e168e4afc73b14f1ac3768
zhsam <samchen0727@gmail.com>
2019-03-27 23:15:15 +0800

    Update: 7-49 advance templating.

--- P3-web-server/src/app.js
+++ P3-web-server/src/app.js
@@ -1,15 +1,18 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname,'../public')
-const viewsPath = path.join(__dirname,'../templates')
+const viewsPath = path.join(__dirname,'../templates/views')
+const partialsPath = path.join(__dirname,'../templates/partials')
 
 // Setup handlebars engine and views locaiton
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
@@ -17,31 +20,32 @@ app.use(express.static(publicDirectoryPath))
 app.get('',(req,res)=>{
     res.render('index',{
         title: 'Weather App',
-        name: 'Andrew Mead'
+        name: 'Sam'
     })
 })
 
 app.get('/about',(req,res)=>{
     res.render('about',{
         title: 'About Page',
-        name: 'Andrew'
+        name: 'Sam'
     })
 })
 
 app.get('/help',(req,res)=>{
     res.render('help',{
         title: 'Help Page',
-        message: 'This is help page'
+        message: 'This is help page',
+        name: 'Sam'
     })
 })
 
-//
-// Goal: Create a template for help page
-//
-// 1. Setup a help template to render a help message to the screen
-// 2. Setup the help route and render the template with an example message
-// 3. Visit the route in the browser
 
+//
+// Goal: Create a partial for the footer
+//
+// 1. Setup the template for the footer partial "Created by XXX"
+// 2. Render the partial at the bottom of all three pages
+// 3. Test your work by visiting all three pages
 
 app.get('/weather', (req, res) => {
     res.send({
