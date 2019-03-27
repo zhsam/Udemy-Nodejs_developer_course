a1cec24e6f9fb099d93e862487d8b0915c4adedf
zhsam <samchen0727@gmail.com>
2019-03-27 22:49:41 +0800

    Update: 4-47 customizing the views directory.

--- P3-web-server/src/7-47_app.js
+++ P3-web-server/src/7-47_app.js
@@ -2,16 +2,9 @@ const path = require('path')
 const express = require('express')
 
 const app = express()
-
-// Define paths for Express config
 const publicDirectoryPath = path.join(__dirname,'../public')
-const viewsPath = path.join(__dirname,'../templates')
 
-// Setup handlebars engine and views locaiton
 app.set('view engine', 'hbs')
-app.set('views', viewsPath)
-
-// Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
 app.get('',(req,res)=>{
